5485aac29ba28f8c0084b31479bd08bdc9fdebfa
Daria Chaenkova <dash.kustowa2010@yandex.ru>
2024-05-18 20:58:11 +0300

    close archive

--- test_file.py
+++ test_file.py
@@ -8,12 +8,14 @@ def test_pdf_file():
     with zipfile.ZipFile("resources/zip.zip", "r") as zip_file:
         page = PdfReader(zip_file.open('pdf.pdf'))
         assert 'Python Testing with pytest' in page.pages[1].extract_text()
+        zip_file.close()
 
 
 def test_xlsx_file():
     with zipfile.ZipFile("resources/zip.zip", "r") as zip_file:
         page = load_workbook(zip_file.open('xlsx.xlsx')).active
         assert 'Mara' in page.cell(row=3, column=2).value
+        zip_file.close()
 
 
 def test_csv_file():
@@ -22,3 +24,4 @@ def test_csv_file():
         csvreader = list(csv.reader(page.splitlines()))
         second_row = csvreader[1]
         assert 'дисплей' in second_row[0]
+        zip_file.close()
